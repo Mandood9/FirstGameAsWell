37a49643f6114f047a8f2e563e6c93964b8b0ce2
Andy Nguyen <andy_nguyen@Andys-MacBook-Pro-4.local>
2019-03-01 03:27:42 -0800

    With attack animation, have to hold attack button

--- Game.py
+++ Game.py
@@ -5,13 +5,19 @@ pygame.init()
 window = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("First Game As Well")
 
-bg = [pygame.image.load('BG1/BGF/BGBack.png'), pygame.image.load('BG1/BGF/BGFront.png'), pygame.image.load('BG1/BGF/CloudsBack.png'), pygame.image.load('BG1/BGF/CloudsFront.png')]
+bg1 = [pygame.image.load('BG1/BGF/BGBack.png'), pygame.image.load('BG1/BGF/BGFront.png'), pygame.image.load('BG1/BGF/CloudsBack.png'), pygame.image.load('BG1/BGF/CloudsFront.png')]
 
 walkRight = [pygame.image.load('MC/adventurer-run-00.png'), pygame.image.load('MC/adventurer-run-01.png'), pygame.image.load('MC/adventurer-run-02.png'), pygame.image.load('MC/adventurer-run-03.png'), pygame.image.load('MC/adventurer-run-04.png'), pygame.image.load('MC/adventurer-run-05.png')]
 
 idle = [pygame.image.load('MC/adventurer-idle-2-00.png'), pygame.image.load('MC/adventurer-idle-2-01.png'), pygame.image.load('MC/adventurer-idle-2-02.png'), pygame.image.load('MC/adventurer-idle-2-03.png')]
 
 jumping = [pygame.image.load('MC/adventurer-smrslt-00.png'), pygame.image.load('MC/adventurer-smrslt-01.png'), pygame.image.load('MC/adventurer-smrslt-02.png'), pygame.image.load('MC/adventurer-smrslt-03.png')]
+
+attack1 = [pygame.image.load('MC/adventurer-attack1-01.png'), pygame.image.load('MC/adventurer-attack1-02.png'), pygame.image.load('MC/adventurer-attack1-03.png'), pygame.image.load('MC/adventurer-attack1-04.png')]
+
+attack2 = [pygame.image.load('MC/adventurer-attack2-00.png'), pygame.image.load('MC/adventurer-attack2-01.png'), pygame.image.load('MC/adventurer-attack2-02.png'), pygame.image.load('MC/adventurer-attack2-03.png'), pygame.image.load('MC/adventurer-attack2-04.png'), pygame.image.load('MC/adventurer-attack2-05.png')]
+
+attack3 = [pygame.image.load('MC/adventurer-attack3-01.png'), pygame.image.load('MC/adventurer-attack3-01.png'), pygame.image.load('MC/adventurer-attack3-02.png'), pygame.image.load('MC/adventurer-attack3-03.png'), pygame.image.load('MC/adventurer-attack3-04.png'), pygame.image.load('MC/adventurer-attack3-05.png')]
 
 clock = pygame.time.Clock()
 
@@ -22,24 +28,41 @@ class mainChar():
         self.width = width * 3
         self.height = height * 3
         self.vel = 10
-        self.standing = True
+
+        self.standingCount = 0
+
         self.facingRight = True
-        self.facingLeft = False
         self.right = False
         self.left = False
-        self.jump = False
-        self.standingCount = 0
         self.walkCount = 0
+
+        self.jump = False
         self.jumpFrame = 0
-        self.jumpCount = 8
+        self.jumpCount = 7
+
+        self.attack1 = False
+        self.attack1Count = 0
+
+#    def counter(self):
+        # if self.walkCount + 1 >= 36:
+        #     self.walkCount = 0
+        # if self.standingCount + 1 >= 36:
+        #     self.standingCount = 0
+        # if self.jumpFrame + 1 >= 36:
+        #     self.jumpFrame = 0
+        # if self.attack1Count + 1 >= 36:
+        #     self.attack1Count = 0
 
     def drawMC(self):
+#        counter()
         if self.walkCount + 1 >= 36:
             self.walkCount = 0
         if self.standingCount + 1 >= 36:
             self.standingCount = 0
         if self.jumpFrame + 1 >= 36:
             self.jumpFrame = 0
+        if self.attack1Count + 1 >= 36:
+            self.attack1Count = 0
 
         if self.right:
             if self.jump:
@@ -62,15 +85,23 @@ class mainChar():
             if self.facingRight:
                 scaled = pygame.transform.scale(jumping[self.jumpFrame//9], (self.width, self.height))
                 window.blit(scaled, (self.x, self.y))
-            if self.facingLeft:
+            else:
                 window.blit(pygame.transform.scale(pygame.transform.flip(jumping[self.jumpFrame//9], True, False), (self.width, self.height)), (mc.x, mc.y))
             self.jumpFrame += 4
+
+        elif self.attack1:
+            if self.facingRight:
+                scaled = pygame.transform.scale(attack1[self.attack1Count//9], (self.width, self.height))
+                window.blit(scaled, (self.x, self.y))
+            else:
+                window.blit(pygame.transform.scale(pygame.transform.flip(attack1[self.attack1Count//9], True, False), (self.width, self.height)), (mc.x, mc.y))
+            self.attack1Count += 4
 
         else:
             if self.facingRight:
                 scaled = pygame.transform.scale(idle[self.standingCount//9], (self.width, self.height))
                 window.blit(scaled, (self.x, self.y))
-            if self.facingLeft:
+            else:
                 window.blit(pygame.transform.scale(pygame.transform.flip(idle[self.standingCount//9], True, False), (self.width, self.height)), (mc.x, mc.y))
             self.standingCount += 4
 
@@ -81,38 +112,46 @@ class mainChar():
             self.right = True
             self.facingRight = True
             self.left = False
-            self.facingLeft = False
             self.x += self.vel
         elif keys[pygame.K_LEFT] and self.x > self.vel:
             self.right = False
             self.facingRight = False
             self.left = True
-            self.facingLeft = True
             self.x -= self.vel
+        # elif keys[pygame.K_1]:
+        #     self.attack1 = True
         else:
             self.right = False
             self.left = False
+            self.attack1 = False
             self.walkCount = 0
+
+        if not(self.attack1):
+            if keys[pygame.K_1]:
+                self.attack1 = True
+        else:
+            # dealDamage()
+            pass
 
         if not(self.jump):
             if keys[pygame.K_SPACE]:
                 self.jump = True
         else:
-            if self.jumpCount >= -9:
+            if self.jumpCount >= -7:
                 neg = 1
                 if self.jumpCount < 0:
                     neg = -1
                 self.y -= (self.jumpCount ** 2) * 0.5 * neg
                 self.jumpCount -= 1
             else:
-                self.jumpCount = 9
+                self.jumpCount = 7
                 self.jump = False
 
 def redrawGameWindow():
-    window.blit(pygame.transform.scale(bg[2], (800, 600)), (0, 0))
-    window.blit(pygame.transform.scale(bg[3], (800, 600)), (0, 0))
-    window.blit(pygame.transform.scale(bg[0], (800, 600)), (0, 0))
-    window.blit(pygame.transform.scale(bg[1], (800, 600)), (0, 0))
+    window.blit(pygame.transform.scale(bg1[2], (800, 600)), (0, 0))
+    window.blit(pygame.transform.scale(bg1[3], (800, 600)), (0, 0))
+    window.blit(pygame.transform.scale(bg1[0], (800, 600)), (0, 0))
+    window.blit(pygame.transform.scale(bg1[1], (800, 600)), (0, 0))
     mc.drawMC()
     pygame.display.update()
 
